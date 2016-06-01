2decefbf4d577461373de9d791e56cb7a6bdf1c5
mlinton99 <mlinton99@hotmail.com>
2016-06-01 23:27:48 +0100

    from Michael Linton

---
+++ GithubMaster.Tests/Controllers/DashboardControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GithubMaster.Controllers;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace GithubMaster.Tests.Controllers
+{
+    [TestClass]
+    public class DashboardControllerTest
+    {
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            DashboardController controller = new DashboardController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+        [TestMethod]
+        public async Task GetResult_Should_Return_List_Of_Repositiers()
+        {
+            string UserName = "natemcmaster";
+
+            // Arrange
+            DashboardController controller = new DashboardController();
+
+            // Act
+            ViewResult result = await controller.GetResult(UserName) as ViewResult;
+
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ GithubMaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubMaster.Startup))]
+namespace GithubMaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
